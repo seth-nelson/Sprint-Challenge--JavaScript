347aa0beb811722248475aead997257efb0c87a1
Seth Nelson <seth.nelson@outlook.com>
2019-12-14 09:37:03 -0800

    partially finished dinosaur section on objects-arrayd.js, but need to fix a few later

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -75,8 +75,13 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
+
+//-----------------------check this one over. Doesn't work.
 const universities = [];
-console.log(universities);
+graduates.forEach(graduate => {
+  universities.push(graduate.university);
+});
+console.log(universities.sort(graduate));
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
@@ -84,13 +89,25 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
+
+//----------------------check this one over later. Doesn't work.
 const contactInfo = [];
+graduates.forEach(student => {
+  let information = `${student.first_name} ${student.email}`;
+    information.toString().split();
+    contactInfo.push(information);
+});
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
-console.log(unisWithUni);
 
+const unisWithUni = [];
+graduates.forEach(graduate => {
+  if(graduate.university.includes('Uni')) {
+    unisWithUni.push(graduate);
+  }
+})
+console.log(unisWithUni);
 
 // ==== ADVANCED Array Methods ====
 
